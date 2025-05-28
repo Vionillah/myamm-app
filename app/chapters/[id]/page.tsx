573669652fea5d13986ecdf1d02f97a54d3e4970
Vionillah <vionillahfauzia@gmail.com>
2025-05-28 20:18:25 +0700

    v.1.09 fixed error PageProps

--- app/chapters/[id]/page.tsx
+++ app/chapters/[id]/page.tsx
@@ -1,40 +1,33 @@
-// import { prisma } from "@/lib/prisma";
-import 'app/globals.css';
-import ChapterClient from './chapterClient';
+// app/chapters/[id]/page.tsx
 import { PrismaClient } from "@prisma/client";
+import ChapterClient from "./chapterClient";
 
 const prisma = new PrismaClient();
 
 const getChaptersByDocumentId = async (documentId: number) => {
-  try {
-    const chapters = await prisma.chapter.findMany({
-      where: {documentId},
-      select: {
-        id: true,
-        name: true,
-        filePath: true,
-        document: true,
-        documentId: true,
-      },
-    });
-    return chapters;
-  } catch (error) {
-    console.error("Error fetching documents:", error);
-    return [];
-  }
+  return await prisma.chapter.findMany({
+    where: { documentId },
+    select: {
+      id: true,
+      name: true,
+      filePath: true,
+      document: true,
+      documentId: true,
+    },
+  });
 };
 
 const getDocuments = async () => {
   return await prisma.document.findMany();
 };
 
-type Params = {
+interface PageProps {
   params: {
     id: string;
   };
-};
+}
 
-export default async function Page({ params }: Params) {
+export default async function Page({ params }: PageProps) {
   const documentId = Number(params.id);
 
   const [chapters, documents] = await Promise.all([
