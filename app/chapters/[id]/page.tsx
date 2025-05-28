96b128e2987b064f01accd0077f6a8f8d5a79cd8
Vionillah <vionillahfauzia@gmail.com>
2025-05-28 20:26:47 +0700

    v.1.12 fixed error

--- app/chapters/[id]/page.tsx
+++ app/chapters/[id]/page.tsx
@@ -1,11 +1,53 @@
-// app/chapters/[id]/page.tsx
-import { PrismaClient } from "@prisma/client";
-import ChapterClient from "./chapterClient";
+import { PrismaClient } from '@prisma/client';
+import ChapterClient from './chapterClient';
 
 const prisma = new PrismaClient();
 
-const getChaptersByDocumentId = async (documentId: number) => {
-  return await prisma.chapter.findMany({
+// type ChapterPageProps = {
+//   params: {
+//     id: string;
+//   };
+// };
+
+// const getChaptersByDocumentId = async (documentId: number) => {
+//   try {
+//     return await prisma.chapter.findMany({
+//       where: { documentId },
+//       select: {
+//         id: true,
+//         name: true,
+//         filePath: true,
+//         document: true,
+//         documentId: true,
+//       },
+//     });
+//   } catch (error) {
+//     console.error('Error fetching chapters:', error);
+//     return [];
+//   }
+// };
+
+// const getDocuments = async () => {
+//   return await prisma.document.findMany();
+// };
+
+// export default async function ChapterPage({ params }: ChapterPageProps) {
+//   const documentId = Number(params.id);
+
+//   const [chapters, documents] = await Promise.all([
+//     getChaptersByDocumentId(documentId),
+//     getDocuments(),
+//   ]);
+
+//   return <ChapterClient chapters={chapters} documents={documents} />;
+// }
+
+// app/chapters/[id]/page.tsx
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const documentId = Number(params.id);
+
+  const chapters = await prisma.chapter.findMany({
     where: { documentId },
     select: {
       id: true,
@@ -15,25 +57,8 @@ const getChaptersByDocumentId = async (documentId: number) => {
       documentId: true,
     },
   });
-};
 
-const getDocuments = async () => {
-  return await prisma.document.findMany();
-};
-
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function Page({ params }: PageProps) {
-  const documentId = Number(params.id);
-
-  const [chapters, documents] = await Promise.all([
-    getChaptersByDocumentId(documentId),
-    getDocuments(),
-  ]);
+  const documents = await prisma.document.findMany();
 
   return <ChapterClient chapters={chapters} documents={documents} />;
 }
